6e619a1bf13f324e721850c45d058fb3a9a9ca78
liketodev1 <freelancetoptalent@gmail.com>
2023-08-22 15:13:39 +0400

    midleware

--- config/database.js
+++ config/database.js
@@ -12,10 +12,10 @@
 module.exports = require('knex')({
     client: 'mysql2',
     connection: {
-        host:'localhost',
-        user: 'Powerd',
+        host:'65.109.11.93',
+        user: 'powerd',
         database: 'powerd1',
-        password: 'powerd',
+        password: 'Powerd@2023',
         multipleStatements: true
     }
 });

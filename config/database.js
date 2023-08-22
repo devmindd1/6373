77f12c5bf523bc9394db4a6d2ca01b3b4029e846
liketodev1 <freelancetoptalent@gmail.com>
2023-08-22 15:07:49 +0400

    midleware

--- config/database.js
+++ config/database.js
@@ -1,21 +1,21 @@
-module.exports = require('knex')({
-    client: 'mysql2',
-    connection: {
-        host : '127.0.0.1',
-        user : 'root',
-        password : '',
-        database : 'powerd',
-        multipleStatements: true
-    }
-});
-
 // module.exports = require('knex')({
 //     client: 'mysql2',
 //     connection: {
-//         host:'localhost',
-//         user: 'chrome',
-//         database: 'chrome_ext',
-//         password: 'eiwojo_hiSh4to9A',
+//         host : '127.0.0.1',
+//         user : 'root',
+//         password : '',
+//         database : 'powerd',
 //         multipleStatements: true
 //     }
 // });
+
+module.exports = require('knex')({
+    client: 'mysql2',
+    connection: {
+        host:'localhost',
+        user: 'Powerd',
+        database: 'powerd1',
+        password: 'powerd',
+        multipleStatements: true
+    }
+});
